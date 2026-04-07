3f13f506ecce5be01028ff5520cdc7d9e522adee
Ivan BOLTENKOV <ivanwrest@gmail.com>
2026-04-08 00:46:57 +0300

    Fix: correct components folder casing

--- types/note.ts
+++ types/note.ts
@@ -1,7 +1,12 @@
+// 
+
+export type NoteTag = 'Work' | 'Personal' | 'Home' | 'Other'; // або ті теги, які використовує API
+
 export interface Note {
   id: string;
   title: string;
   content: string;
-  tag: string;
+  tag: NoteTag; // або просто string, якщо не хочеш морочитися з юніоном
   createdAt: string;
+  updatedAt: string;
 }
