3c2d89b6d966b76e165c70420f4f6b978c5cf3a7
Ivan BOLTENKOV <ivanwrest@gmail.com>
2026-04-23 01:42:08 +0300

    Final

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -2,7 +2,7 @@
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { deleteNote } from '@/lib/api'; 
+import { deleteNote } from "@/lib/api/clientApi";
 import type { Note } from '@/types/note';
 import css from './NoteList.module.css';
 import Link from "next/link";
